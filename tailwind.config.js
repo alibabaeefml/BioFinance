ac31311644323453822602cff021962c82a78cd5
alibabaeefml <alisource79@gmail.com>
2023-01-16 15:59:39 +0330

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,17 @@ module.exports = {
         yellow: "#FFCC29",
         black: "#000000",
         lightgreen1: "#E8FFD6",
-        lightgreen2:"#F4FAEE",
+        lightgreen2: "#F4FAEE",
         lightyellow: "#1E3163",
         silver: "#F0F0F0",
-        white70:"rgba(255,255,255,0.5)"
+        white70: "rgba(255,255,255,0.5)",
       },
       container: {
         center: true,
       },
-      screens:{
-        "2xl":"1366px"
-      }
+      screens: {
+        "2xl": "1366px",
+      },
     },
   },
   plugins: [],

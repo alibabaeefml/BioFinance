bd156ca01e50ca49ed224a498ff41f4e4acbb2dc
alibabaeefml <alisource79@gmail.com>
2023-01-22 14:47:53 +0330

    commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,5 @@
 // navbar background
-$("document").ready(()=>{
+$("document").ready(() => {
   if ($(window).scrollTop() > 80) {
     if (!$(".navbar-div.scrolled").length) {
       $(".navbar-div").addClass("scrolled");
@@ -7,15 +7,20 @@ $("document").ready(()=>{
   } else {
     $(".navbar-div").removeClass("scrolled");
   }
-    $(window).scroll(() => {
-        if (innerWidth > 640) {
-          if ($(window).scrollTop() > 80) {
-            if (!$(".navbar-div.scrolled").length) {
-              $(".navbar-div").addClass("scrolled");
-            }
-          } else {
-            $(".navbar-div").removeClass("scrolled");
-          }
+  $(window).scroll(() => {
+    if (innerWidth > 640) {
+      if ($(window).scrollTop() > 80) {
+        if (!$(".navbar-div.scrolled").length) {
+          $(".navbar-div").addClass("scrolled");
         }
-      });
-})
+      } else {
+        $(".navbar-div").removeClass("scrolled");
+      }
+    }
+    if ($(window).scrollTop() > 80) {
+      $("#back_to_top_btn").show();
+    } else {
+      $("#back_to_top_btn").hide();
+    }
+  });
+});
